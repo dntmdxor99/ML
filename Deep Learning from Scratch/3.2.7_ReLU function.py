7ab8b6197b359761258846be991e41c01a5b12cd
Woo <82219614+dntmdxor99@users.noreply.github.com>
2022-05-07 16:11:20 +0900

    Create 3.2.7_ReLU function.py

---
+++ Deep Learning from Scratch/3.2.7_ReLU function.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def ReLU(x):
+    return np.maximum(0, x)
+
+
+x = np.array([-1.0, 1.0, 2.0])
+print(ReLU(x))
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = ReLU(x)
+plt.plot(x, y)
+plt.ylim(-0.1, 5.1)
+plt.show()
