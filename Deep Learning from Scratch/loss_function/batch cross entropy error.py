c92d5ca2f5425b14a00f59c6a8830a50d3d2afad
Woo <82219614+dntmdxor99@users.noreply.github.com>
2022-05-09 11:54:58 +0900

    Create batch cross entropy error.py

---
+++ Deep Learning from Scratch/loss_function/batch cross entropy error.py
@@ -0,0 +1,30 @@
+import numpy as np
+from mnist import load_mnist
+
+
+def batch_cross_entropy_error(pred, target):
+    if pred.ndim == 1:
+        target = target.reshape(1, target.size)
+        pred = pred.reshape(1, pred.size)
+
+    batch_size = pred.shape[0]
+    return -np.sum(target * np.log(pred + 1e-7)) / batch_size
+
+
+if __name__ == "__main__":
+    (input_train, target_train), (input_test, target_test) = load_mnist(normalize=True, one_hot_label=True)
+
+    train_size = input_train.shape[0]
+    batch_size = 10
+    batch_mask = np.random.choice(train_size, batch_size)
+
+    input_batch = input_train[batch_mask]
+    target_batch = target_train[batch_mask]
+    pred_batch = np.array([[0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+                           [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+                           [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1], [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1],
+                           [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]])
+
+    error = batch_cross_entropy_error(pred_batch, target_batch)
+
+    print(error)
