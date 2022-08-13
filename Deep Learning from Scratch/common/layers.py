4ba6248e5f8db07b3aacfd1e83c85f2c6da49ba5
dntmdxor99 <dntmdxor99@naver.com>
2022-08-13 19:49:47 +0900

    .

--- Deep Learning from Scratch/common/layers.py
+++ Deep Learning from Scratch/common/layers.py
@@ -1,3 +1,4 @@
+from turtle import forward
 import numpy as np
 
 
@@ -37,3 +38,28 @@ class Sigmoid:
         dx = dout * (1.0 - self.out) * self.out
 
         return dx
+
+
+class Affine:
+    def __init__(self, W, b) -> None:
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+
+        return out
+
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis = 0)
+
+        return dx
+
