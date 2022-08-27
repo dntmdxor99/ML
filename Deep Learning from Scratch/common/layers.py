dc7c45bbfae3d804134977f3af805616c27e3e8a
dntmdxor99 <dntmdxor99@naver.com>
2022-08-27 17:28:57 +0900

    .

--- Deep Learning from Scratch/common/layers.py
+++ Deep Learning from Scratch/common/layers.py
@@ -1,6 +1,7 @@
 import numpy as np
 from Activation_Function.Softmax_Function import softmax
 from Loss_Function.cross_entropy_error import cross_entropy
+from utils import im2col, col2im
 
 
 class Relu:
@@ -84,3 +85,44 @@ class SoftmaxWithLoss:
         dx = (self.y - self.t) / batch_size
 
         return dx
+
+
+class Convolution:
+    def __init__(self, W, b, stride = 1, pad = 0) -> None:
+        self.W = W
+        self.b = b
+        self.stride = stride
+        self.pad = pad
+
+
+    def forward(self, x):
+        FN, C, FH, FW = self.W.shape
+        N, C, H, W = x.shape
+        out_h = int(1 + (H + 2 * self.pad - FH) / self.stride)
+        out_w = int(1 + (W + 2 * self.pad - FW) / self.stride)
+
+        col = im2col(x, FH, FW, stride = self.stride, pad = self.pad)
+        col_W = self.W.reshape(FN , -1).T
+        out = np.dot(col, col_W) + self.b
+
+        out = out.reshape(N, out_h, out_w, -1).transpose(0, 3, 1, 2)
+
+        return out
+
+
+    def backward(self, dout):
+        FN, C, FH, FW = self.W.shape
+        dout = dout.transpose(0,2,3,1).reshape(-1, FN)
+
+        self.db = np.sum(dout, axis=0)
+        self.dW = np.dot(self.col.T, dout)
+        self.dW = self.dW.transpose(1, 0).reshape(FN, C, FH, FW)
+
+        dcol = np.dot(dout, self.col_W.T)
+        dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
+
+        return dx
+
+
+if __name__ == "__main__":
+    pass
