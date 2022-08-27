567c6a1ded1dfce580cf90393cfe729e1262e700
dntmdxor99 <dntmdxor99@naver.com>
2022-08-27 19:17:02 +0900

    .

--- Deep Learning from Scratch/common/layers.py
+++ Deep Learning from Scratch/common/layers.py
@@ -1,3 +1,4 @@
+from multiprocessing import pool
 from typing import Collection
 import numpy as np
 from Activation_Function.Softmax_Function import softmax
@@ -123,6 +124,44 @@ class Convolution:
         dx = col2im(dcol, self.x.shape, FH, FW, self.stride, self.pad)
 
         return dx
+
+
+class Pooling:
+    def __init__(self, pool_h, pool_w, stride = 1, pad = 0) -> None:
+        self.pool_h = pool_h
+        self.pool_w = pool_w
+        self.stride = stride
+        self.pad = pad
+
+
+    def forward(self, x):
+        N, C, H, W = x.shape
+        out_h = int(1 + (H - self.pool_h) / self.stride)
+        out_w = int(1 + (W - self.pool_w) / self.stride)
+
+        col = im2col(x, self.pool_h, self.pool_w, stride = self.stride, pad = self.pad)
+        col = col.reshape(-1, self.pool_h * self.pool_w)
+
+        out = np.max(axis = 1)
+
+        out = out.reshape(N, out_h, out_w, C).transpose(0, 3, 1, 2)
+
+        return out
+
+
+    
+    def backward(self, dout):
+        dout = dout.transpose(0, 2, 3, 1)
+        
+        pool_size = self.pool_h * self.pool_w
+        dmax = np.zeros((dout.size, pool_size))
+        dmax[np.arange(self.arg_max.size), self.arg_max.flatten()] = dout.flatten()
+        dmax = dmax.reshape(dout.shape + (pool_size,)) 
+        
+        dcol = dmax.reshape(dmax.shape[0] * dmax.shape[1] * dmax.shape[2], -1)
+        dx = col2im(dcol, self.x.shape, self.pool_h, self.pool_w, self.stride, self.pad)
+        
+        return 
 
 
 if __name__ == "__main__":
